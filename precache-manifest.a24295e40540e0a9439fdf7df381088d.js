4154d045797faeb5315b69a26e4a061fb4c20084
ice-rahul <meet4g@gmail.com>
2021-02-11 12:02:52 +0530

    rebuild to make it compatible with github pages

---
+++ precache-manifest.a24295e40540e0a9439fdf7df381088d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "785f32f0ffa8d67dc26a0c1896205318",
+    "url": "/fast-fingers/index.html"
+  },
+  {
+    "revision": "25de6578a4782020516e",
+    "url": "/fast-fingers/static/css/main.defc5372.chunk.css"
+  },
+  {
+    "revision": "bc216440efeac41dfadb",
+    "url": "/fast-fingers/static/js/2.6b86ca83.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/fast-fingers/static/js/2.6b86ca83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25de6578a4782020516e",
+    "url": "/fast-fingers/static/js/main.5110a38e.chunk.js"
+  },
+  {
+    "revision": "5641e1afe3c3ed46422e",
+    "url": "/fast-fingers/static/js/runtime-main.dded97a4.js"
+  },
+  {
+    "revision": "0b28710a605aed91211bd25a19081bae",
+    "url": "/fast-fingers/static/media/Gotham Medium Regular.0b28710a.ttf"
+  },
+  {
+    "revision": "b54724f54d4dd3f6796e3c4cc422f998",
+    "url": "/fast-fingers/static/media/GothamBook.b54724f5.ttf"
+  },
+  {
+    "revision": "d4ccc48c66b7c07c692ae80633360821",
+    "url": "/fast-fingers/static/media/breamcatcher.d4ccc48c.ttf"
+  },
+  {
+    "revision": "465bb1673f9f4de64d7cc1d7fa086985",
+    "url": "/fast-fingers/static/media/cross-icon.465bb167.svg"
+  },
+  {
+    "revision": "883c878912777fef1d9c11aeb602ed01",
+    "url": "/fast-fingers/static/media/drop.883c8789.svg"
+  },
+  {
+    "revision": "e32312a7bf00e36cc88b3787b8b36b48",
+    "url": "/fast-fingers/static/media/fast-fingers-background.e32312a7.png"
+  },
+  {
+    "revision": "0cd364ed5f73e9674fabbc73453de723",
+    "url": "/fast-fingers/static/media/gamepad-icon.0cd364ed.svg"
+  },
+  {
+    "revision": "1578a34cbe02e9604ed3c3b3581bf980",
+    "url": "/fast-fingers/static/media/home-icon.1578a34c.svg"
+  },
+  {
+    "revision": "9c1f982035aaa27323828a36b6811466",
+    "url": "/fast-fingers/static/media/keyboard-icon.9c1f9820.svg"
+  },
+  {
+    "revision": "9f77e39e8dbf2ec67f77649104be2814",
+    "url": "/fast-fingers/static/media/lasercorpsgrad.9f77e39e.ttf"
+  },
+  {
+    "revision": "c589b8d1b4823b668a4780e8b65a1bf2",
+    "url": "/fast-fingers/static/media/person-icon.c589b8d1.svg"
+  },
+  {
+    "revision": "36e8117ef5896ddb8e8e082c814d351f",
+    "url": "/fast-fingers/static/media/play-icon.36e8117e.svg"
+  },
+  {
+    "revision": "7c71c1abb2fa43afa27c621e1f91e9bb",
+    "url": "/fast-fingers/static/media/reload-icon.7c71c1ab.svg"
+  }
+]);
